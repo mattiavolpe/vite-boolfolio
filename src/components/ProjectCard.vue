7909da1f3070580d238f531c89c7b79bd30f403d
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-14 14:55:07 +0200

    CHANGE project card layout

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,22 +13,20 @@ export default {
   <div class="card border-0 bg-black box_shadow_custom_green h-100">
     <img class="card-img-top" :src="host + imagesPath + project.image">
     <div class="card-header text-center showcase_project_title">
-        <a :href="project.repositoryUrl" target="_blank" class="text_custom_green">
-            <h5 class="mb-0 fw-bold text_custom_green">{{ project.name }}</h5>
-        </a>
+      <h4 class="mb-0 fw-bold text_custom_green">{{ project.name }}</h4>
     </div>
     <div class="card-body">
       <template v-if="project.repositoryUrl">
-        <h6 class="text_custom_green fw-bold mb-0">Repository URL:</h6>
+        <h6 class="text_custom_green fw-bold mb-0">./Repository URL:</h6>
         <a :href="project.repositoryUrl" target="_blank" class="text-white fw-normal mb-2 d-block">{{ project.repositoryUrl }}</a>
         <hr class="border_custom_green">
       </template>
-      <h6 class="text_custom_green fw-bold" v-if="project.type">Project type:
+      <h6 class="text_custom_green fw-bold" v-if="project.type">./Project type:
         <span class="text-white fw-normal">{{ project.type.name }}</span>
         <hr class="border_custom_green">
       </h6>
       <template v-if="project.technologies">
-        <h6 class="text_custom_green fw-bold mb-0">Used technologies:</h6>
+        <h6 class="text_custom_green fw-bold mb-0">./Used technologies:</h6>
         <ul class="text_custom_green fw-bold list-unstyled"> 
           <li class="text-white fw-normal" v-for="technology in project.technologies">{{ technology.name }}</li>
         </ul>

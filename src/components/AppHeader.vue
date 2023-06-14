bdb90fc917568de805ff57cad91ee7a166325198
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-14 16:01:37 +0200

    ADD AppHome view

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,8 +8,11 @@ export default {
 <header class="position-sticky top-0 start-0">
   <nav class="navbar navbar-expand-md navbar-dark bg-black">
     <div class="container">
-      <a class="navbar-brand me-0 d-flex align-items-center" href="/">
+      <router-link :to="{ name: 'home' }">
         <div class="app_logo">$ > Boolfolio</div>
+      </router-link>
+      <a class="navbar-brand me-0 d-flex align-items-center" href="/">
+        sdfafdsf
       </a>
     </div>
   </nav>

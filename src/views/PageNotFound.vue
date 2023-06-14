783098eac55eb7a072614a27ea50d0a9e3e5972f
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-14 18:30:04 +0200

    ADD link to home in 404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -5,7 +5,8 @@ export default {
 </script>
 
 <template>
-  <h1 class="position-fixed start-50 top-50 translate-middle py-5 text_custom_green">
-    404 | NOT FOUND
-  </h1>
+  <div class="position-fixed start-50 top-50 translate-middle py-5 text-center text_custom_green">
+    <h1>404 | NOT FOUND</h1>
+    <router-link class="text_custom_green" :to="{ name: 'home' }">Go back to HOME</router-link>
+  </div>
 </template>

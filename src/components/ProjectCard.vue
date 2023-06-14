c1be5b5faa8b5908a0fe5d6ef59df4d17753dc88
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-14 09:30:40 +0200

    CHANGE card output order

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -18,21 +18,21 @@ export default {
         </a>
     </div>
     <div class="card-body">
-        <h6 class="text_custom_green fw-bold" v-if="project.type">Project type:
-          <span class="text-white fw-normal">{{ project.type.name }}</span>
-          <hr class="border_custom_green">
-        </h6>
-        <template v-if="project.repositoryUrl">
-          <h6 class="text_custom_green fw-bold mb-0">Repository URL:</h6>
-          <a :href="project.repositoryUrl" target="_blank" class="text-white fw-normal mb-2 d-block">{{ project.repositoryUrl }}</a>
-          <hr class="border_custom_green">
-        </template>
-        <template v-if="project.technologies">
-            <h6 class="text_custom_green fw-bold mb-0">Used technologies:</h6>
-            <ul class="text_custom_green fw-bold list-unstyled"> 
-                <li class="text-white fw-normal" v-for="technology in project.technologies">{{ technology.name }}</li>
-            </ul>
-        </template>
+      <template v-if="project.repositoryUrl">
+        <h6 class="text_custom_green fw-bold mb-0">Repository URL:</h6>
+        <a :href="project.repositoryUrl" target="_blank" class="text-white fw-normal mb-2 d-block">{{ project.repositoryUrl }}</a>
+        <hr class="border_custom_green">
+      </template>
+      <h6 class="text_custom_green fw-bold" v-if="project.type">Project type:
+        <span class="text-white fw-normal">{{ project.type.name }}</span>
+        <hr class="border_custom_green">
+      </h6>
+      <template v-if="project.technologies">
+        <h6 class="text_custom_green fw-bold mb-0">Used technologies:</h6>
+        <ul class="text_custom_green fw-bold list-unstyled"> 
+          <li class="text-white fw-normal" v-for="technology in project.technologies">{{ technology.name }}</li>
+        </ul>
+      </template>
     </div>
   </div>
 </template>

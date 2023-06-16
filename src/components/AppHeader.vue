712498696fd3a63cbb4f6fff041c9aca9bed9531
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-16 18:24:56 +0200

    CHANGE logo destination

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,9 +8,9 @@ export default {
 <header class="position-sticky top-0 start-0">
   <nav class="navbar navbar-expand-md navbar-dark bg-black">
     <div class="container">
-      <router-link class="text-decoration-none" :to="{ name: 'home' }">
+      <a class="text-decoration-none" href="/">
         <div class="app_logo">$ > Boolfolio</div>
-      </router-link>
+      </a>
     </div>
   </nav>
 </header>

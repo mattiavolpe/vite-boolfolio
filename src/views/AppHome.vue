b2b89fa050f1c6c802b236961300014058ca9fea
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-17 18:13:10 +0200

    STARTING about section

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,13 +1,16 @@
 <script>
 import AppJumbotron from '../components/AppJumbotron.vue';
+import AboutSection from '../components/AboutSection.vue';
 export default {
   name: "AppHome",
   components: {
     AppJumbotron,
+    AboutSection,
   }
 }
 </script>
 
 <template>
   <AppJumbotron />
+  <AboutSection />
 </template>

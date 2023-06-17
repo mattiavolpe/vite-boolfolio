188a2bbc8f8887467d9f0ca38a17b74dc7c310e8
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-17 15:54:06 +0200

    MINOR change in App.vue and 2 more utility classes

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
 import AppHeader from "./components/AppHeader.vue";
 
 export default {
-    name: "App",
-    components: {
-        AppHeader,
-    },
+  name: "App",
+  components: {
+    AppHeader,
+  },
 }
 </script>
 
 <template>
-    <AppHeader/>
-    <router-view></router-view>
+  <AppHeader/>
+  <router-view></router-view>
 </template>
 
 <style lang="scss">

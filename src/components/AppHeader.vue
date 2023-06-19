e338f8ab3bf8e68c3c0a91fe3984ed9b081586b0
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-19 22:42:32 +0200

    CHANGE logo liink destination

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,9 +17,9 @@ export default {
   <header class="position-sticky top-0 start-0 py-3 bg_dark_custom">
     <nav class="navbar navbar-expand-md p-0">
       <div class="container">
-        <a class="navbar-brand p-0" href="/">
+        <router-link :to="{name: 'home'}" class="navbar-brand p-0">
           <img height="50" :src="state.getImageUrl('logo_light_optimized.png')" alt="MV Site Logo">
-        </a>
+        </router-link>
         <button class="navbar-toggler border-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle menu">
           <font-awesome-icon icon="fa-solid fa-bars" class="fa-xl text_light_custom" />
         </button>
@@ -32,7 +32,7 @@ export default {
               <router-link :to="{name: 'projects'}" class="nav-link text_light_custom fw-medium" aria-current="page">Projects</router-link>
             </li>
             <li class="nav-item">
-              <router-link :to="{name: 'contacts'}" class="nav-link text_light_custom fw-medium" aria-current="page">Contacts</router-link>
+              <router-link :to="{name: 'contacts'}" class="nav-link text_light_custom fw-medium" aria-current="page">Contact me</router-link>
             </li>
           </ul>
         </div>

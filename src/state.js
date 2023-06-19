a6d63390ad22f9ac0eb594850055f6818afe041d
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-19 22:42:01 +0200

    ADD api endpoint to retrieve the 3 newest projects

--- src/state.js
+++ src/state.js
@@ -2,8 +2,9 @@ import { reactive } from "vue";
 
 export const state = new reactive({
   host: "http://127.0.0.1:8000/",
-  projectsEndpoint: "api/projects",
-  technologiesEndpoint: "api/technologies",
+  projectsEndpoint: "api/projects/",
+  latestProjectsEndpoint: "api/latestProjects/",
+  technologiesEndpoint: "api/technologies/",
   imagesPath: "storage/",
 
   technologies: [],

f2a24a83e3aac246b61841b261c1baeb475063b8
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-19 14:55:42 +0200

    ADD skills section

---
+++ src/components/SkillsSection.vue
@@ -0,0 +1,38 @@
+<script>
+import { state } from '../state';
+import SectionTitle from './SectionTitle.vue';
+export default {
+  name: "SkillsSection",
+  components: {
+    SectionTitle,
+  },
+  data() {
+    return {
+      state,
+    }
+  },
+  mounted() {
+    state.skillsSectionEl = document.getElementById("skillsSection");
+  }
+}
+</script>
+
+<template>
+  <div id="skillsSection" class="pb-5">
+    <div class="container">
+      <SectionTitle>MY SKILLS</sectionTitle>
+      <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6 g-3 justify-content-center">
+        <div class="col" v-for="technology in state.alphabeticalTechnologies">
+          <div class="technologyWrapper rounded-3 p-2">
+            <img :src="state.host + state.imagesPath + technology.logo" :alt="technology.name + ' Logo'">
+            <p class="mb-0 mt-2 fw-semibold">{{ technology.name }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/_SkillsSection.scss";
+</style>

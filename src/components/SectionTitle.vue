042fac095e7ee499f4c2578ac663119a60e58aad
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-19 14:55:20 +0200

    ADD SectionTitle with slot for different content

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "SectionTitle",
+}
+</script>
+
+<template>
+  <h2 class="fw-bold text_accent_custom mb-3 text-uppercase">
+    <slot></slot>
+  </h2>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 2rem;
+}
+</style>

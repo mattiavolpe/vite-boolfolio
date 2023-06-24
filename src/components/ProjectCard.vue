b2bb59fcf350bab2260600225ee632235e8f3bca
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-24 15:58:48 +0200

    ADD ProjectCard component

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,32 +10,17 @@ export default {
 </script>
 
 <template>
-  <div class="card border-0 bg-black box_shadow_custom_green h-100">
-    <router-link :to="{ name: 'singleProject', params: {slug: project.slug} }">
-      <img class="card-img-top" :src="host + imagesPath + project.image" :alt="`${project.name} image`">
-    </router-link>
-    <div class="card-header text-center showcase_project_title">
-      <router-link class="text-decoration-none" :to="{ name: 'singleProject', params: {slug: project.slug} }">
-        <h4 class="mb-0 fw-bold text_custom_green">{{ project.name }}</h4>
-      </router-link>
+  <router-link :to="{name: 'singleProject', params:{slug: project.slug}}">
+    <div class="projectWrapper rounded-3 p-2 position-relative">
+      <img :src="host + imagesPath + project.image" :alt="project.name + ' Image'">
+      <div class="infos">
+        <p class="text_dark_custom text-uppercase" v-if="project.type">{{ project.type.name }}</p>
+        <button class="singleProjectLink text-decoration-none py-2 px-3 rounded bg_dark_custom text_accent_custom fw-bold">See details</button>
+      </div>
     </div>
-    <div class="card-body">
-      <template v-if="project.repositoryUrl">
-        <h6 class="text_custom_green fw-bold mb-0">./Repository URL:</h6>
-        <a :href="project.repositoryUrl" target="_blank" class="text-white fw-normal mb-2 d-block">{{ project.repositoryUrl }}</a>
-        <hr class="border_custom_green">
-      </template>
-      <h6 class="text_custom_green fw-bold" v-if="project.type">./Project type:
-        <span class="text-white fw-normal">{{ project.type.name }}</span>
-        <hr class="border_custom_green">
-      </h6>
-      <template v-if="project.technologies">
-        <h6 class="text_custom_green fw-bold mb-0">./Used technologies:</h6>
-        <ul class="text_custom_green fw-bold list-unstyled mb-0"> 
-          <li class="text-white fw-normal" v-for="technology in project.technologies">{{ technology.name }}</li>
-        </ul>
-      </template>
-    </div>
-    <router-link class="mx-3 mb-3 text-center text-white rounded px-3 py-1 text-decoration-none fw-bold border_custom_green bg_custom_green" :to="{ name: 'singleProject', params: {slug: project.slug}}">PROJECT DETAILS</router-link>
-  </div>
+  </router-link>
 </template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/_ProjectCard.scss";
+</style>

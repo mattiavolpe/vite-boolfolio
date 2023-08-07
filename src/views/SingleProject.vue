9ce5b8091532847e928721d1ed9ed4ddea438a34
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-08-07 19:16:04 +0200

    FIX margins

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -45,25 +45,26 @@ export default {
       <img class="img-fluid" :src="state.host + state.imagesPath + project.image" :alt="`${project.name} image`">
     </div>
     <template v-if="project.repositoryUrl">
-        <h3 class="text_accent_custom fw-bold">Repository URL:</h3>
-        <a id="repositoryUrl" :href="project.repositoryUrl" target="_blank" class="text_light_custom fw-normal mb-2 d-block fs-4">{{ project.repositoryUrl }}</a>
-        <hr>
-      </template>
-      <h3 class="text_accent_custom fw-bold" v-if="project.type">Project type:
-        <span class="text_light_custom fw-normal d-block fs-4">{{ project.type.name }}</span>
-        <hr>
-      </h3>
-      <template v-if="project.technologies">
-        <h3 class="text_accent_custom fw-bold">Used technologies:</h3>
-        <ul class="fw-bold list-unstyled row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6 g-3 justify-content-center"> 
-          <li class="col" v-for="technology in project.technologies">
-            <div class="technologyWrapper rounded-3 p-2">
-              <img :src="state.host + state.imagesPath + technology.logo" :alt="technology.name + ' Logo'">
-              <p class="mb-0 mt-2 fw-normal">{{ technology.name }}</p>
-            </div>
-          </li>
-        </ul>
-      </template>
+      <h3 class="text_accent_custom fw-bold">Repository URL:</h3>
+      <a id="repositoryUrl" :href="project.repositoryUrl" target="_blank" class="text_light_custom fw-normal mb-2 d-block fs-4">{{ project.repositoryUrl }}</a>
+      <hr>
+    </template>
+    <template v-if="project.type">
+      <h3 class="text_accent_custom fw-bold" >Project type:</h3>
+      <span class="text_light_custom fw-normal d-block fs-4">{{ project.type.name }}</span>
+      <hr>
+    </template>
+    <template v-if="project.technologies">
+      <h3 class="text_accent_custom fw-bold">Used technologies:</h3>
+      <ul class="fw-bold list-unstyled row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6 g-3 justify-content-center"> 
+        <li class="col" v-for="technology in project.technologies">
+          <div class="technologyWrapper rounded-3 p-2">
+            <img :src="state.host + state.imagesPath + technology.logo" :alt="technology.name + ' Logo'">
+            <p class="mb-0 mt-2 fw-normal">{{ technology.name }}</p>
+          </div>
+        </li>
+      </ul>
+    </template>
   </div>
   <PageLoader v-else>
     LOADING PROJECT DETAILS...

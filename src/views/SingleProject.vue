d619848f8a7e04fc8885d7ea1a0d2c39e1224bb3
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-08-07 19:00:36 +0200

    CHANGE single project layout

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -2,11 +2,13 @@
 import { state } from "../state";
 import axios from 'axios';
 import PageLoader from '../components/PageLoader.vue';
+import SectionTitle from '../components/SectionTitle.vue';
 
 export default {
     name: "SingleProject",
     components: {
       PageLoader,
+      SectionTitle
     },
     data() {
         return {
@@ -36,23 +38,27 @@ export default {
 
 <template>
   <div class="container py-5" v-if="project && !loading">
-    <h1 class="text-center text_custom_green mb-0 fw-bold">{{ project.name }}</h1>
+    <SectionTitle>
+        {{ project.name }}
+    </SectionTitle>
     <div class="text-center py-4">
       <img class="img-fluid" :src="state.host + state.imagesPath + project.image" :alt="`${project.name} image`">
     </div>
     <template v-if="project.repositoryUrl">
-        <h3 class="text_custom_green fw-bold mb-0">./Repository URL:</h3>
-        <a id="repositoryUrl" :href="project.repositoryUrl" target="_blank" class="text-white fw-normal mb-2 d-block fs-4">{{ project.repositoryUrl }}</a>
+        <h3 class="text_accent_custom fw-bold mb-0">Repository URL:</h3>
+        <a id="repositoryUrl" :href="project.repositoryUrl" target="_blank" class="text_light_custom fw-normal mb-2 d-block fs-4">{{ project.repositoryUrl }}</a>
         <hr class="border_custom_green">
       </template>
-      <h3 class="text_custom_green fw-bold" v-if="project.type">./Project type:
-        <span class="text-white fw-normal fs-4">{{ project.type.name }}</span>
+      <h3 class="text_accent_custom fw-bold" v-if="project.type">Project type:
+        <span class="text_light_custom fw-normal fs-4">{{ project.type.name }}</span>
         <hr class="border_custom_green">
       </h3>
       <template v-if="project.technologies">
-        <h3 class="text_custom_green fw-bold mb-0">./Used technologies:</h3>
-        <ul class="text_custom_green fw-bold list-unstyled"> 
-          <li class="text-white fw-normal fs-4" v-for="technology in project.technologies">{{ technology.name }}</li>
+        <h3 class="text_accent_custom fw-bold mb-0">Used technologies:</h3>
+        <ul class="fw-bold list-unstyled"> 
+          <li class="text_light_custom fw-normal fs-4" v-for="technology in project.technologies">
+            <img :src="state.host + state.imagesPath + technology.logo" :alt="technology.name + ' Logo'">
+          </li>
         </ul>
       </template>
   </div>
